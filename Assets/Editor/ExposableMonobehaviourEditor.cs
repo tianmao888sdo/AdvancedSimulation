f168ad3d637bc46f0ddd0c8f316a0ed4d39fa9ee
tianmao888sdo <feiyingshendao@163.com>
2016-12-15 14:01:59 +0800

    map scale is over

---
+++ Assets/Editor/ExposableMonobehaviourEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+[CustomEditor(typeof(MonoBehaviour), true)]
+public class ExposableMonobehaviourEditor : Editor
+{
+    MonoBehaviour m_Instance;
+    PropertyField[] m_fields;
+
+    public virtual void OnEnable()
+    {
+        m_Instance = target as MonoBehaviour;
+        m_fields = ExposeProperties.GetProperties(m_Instance);
+    }
+
+    public override void OnInspectorGUI()
+    {
+        if (m_Instance == null)
+            return;
+        this.DrawDefaultInspector();
+        ExposeProperties.Expose(m_fields);
+    }
+}
